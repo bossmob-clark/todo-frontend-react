4b4108f0afa084dfb3e59d8fbf98a60d313b8a95
clark <>
2021-07-15 01:03:12 +0900

    (feature) 회원가입/로그인 작업 (임시)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,112 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import './Login.css';
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import Dialog from "@material-ui/core/Dialog";
+import {useHistory} from "react-router-dom";
+import Slide from "@material-ui/core/Slide";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Login = () => {
+    const history = useHistory();
+
+    const [loginAlertDatas, setOpenLoginAlert] = React.useState({
+        open: false,
+        title: '',
+        msg: ''
+    });
+
+    const handleCloseLoginAlert = () => {
+        setOpenLoginAlert({
+            open: false
+        });
+    }
+
+    const [inputs, setInputs] = useState({
+        id: '',
+        pw: ''
+    });
+
+    const {id, pw} = inputs;
+
+    const onChangeLoginInput = (e) => {
+        const {name, value} = e.target;
+
+        const nextInputs = {
+            ...inputs,
+            [name]: value,
+        }
+
+        setInputs(nextInputs)
+    }
+
+    function handleLoginClick() {
+        if (localStorage.getItem(inputs.id)) {
+            const pw = localStorage.getItem(inputs.id);
+            if (pw === inputs.pw) {
+                sessionStorage.setItem('currId', inputs.id);
+                history.push('/todolist');
+            } else {
+                setOpenLoginAlert({
+                    open: true,
+                    title: '로그인 실패',
+                    msg: '비밀번호가 다릅니다.'
+                });
+            }
+        } else {
+            setOpenLoginAlert({
+                open: true,
+                title: '로그인 실패',
+                msg: 'ID가 없어용'
+            });
+        }
+    }
+
     return (
         <div>
-            <h2>Login</h2>
+            <Container maxWidth={"sm"} className="text-center">
+                <h2>Login</h2>
+                <br></br>
+                <form>
+                    <TextField label={"id"} onChange={onChangeLoginInput} name={"id"} value={id}></TextField>
+                    <br></br>
+                    <TextField label={"password"} type={"password"} onChange={onChangeLoginInput} name={"pw"}
+                               value={pw}></TextField>
+                </form>
+                <br></br>
+                <Button variant={"contained"} color={"primary"} onClick={handleLoginClick}>Login</Button>
+            </Container>
+
+            <Dialog
+                open={loginAlertDatas.open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleCloseLoginAlert}
+                aria-labelledby="alert-dialog-slide-title"
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle id="alert-dialog-slide-title">{loginAlertDatas.title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        {loginAlertDatas.msg}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseLoginAlert} color="primary">
+                        확인
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
-};
+}
 
 export default Login;
